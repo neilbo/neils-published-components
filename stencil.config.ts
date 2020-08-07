5ab14bda2bb87837a0c4d7d193a1241af58bf7da
Neil <neilbo21@gmail.com>
2020-08-07 14:35:56 +1000

    try rename to match component config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'neils-published-components',
+  namespace: 'product-card',
   taskQueue: 'async',
   outputTargets: [
     {
