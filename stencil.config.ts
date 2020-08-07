3dc15dbf9e10c7897e9fd281dc897f5fcadfc2f2
Neil <neilbo21@gmail.com>
2020-08-07 14:48:36 +1000

    Revert "try rename to match component config"
    This reverts commit 5ab14bda2bb87837a0c4d7d193a1241af58bf7da.

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'product-card',
+  namespace: 'neils-published-components',
   taskQueue: 'async',
   outputTargets: [
     {
